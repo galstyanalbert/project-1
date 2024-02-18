2e32c781b9370b8947666df77e9d62215c9abda1
galstyanalbert <160430825+galstyanalbert@users.noreply.github.com>
2024-02-19 01:34:06 +0400

    Add files via upload

---
+++ Onepage Restaurant/JS/script.js
@@ -0,0 +1,13 @@
+function menu_button(){
+	document.getElementById('op_menu').style.display='block';
+}
+function close_button(){
+	document.getElementById('op_menu').style.display='none';
+}
+
+function con_button(){
+	document.getElementById('con_menu').style.display='block';
+}
+function close_button2(){
+	document.getElementById('con_menu').style.display='none';
+}
